8d7cf069ab2894411b20cde6a7518ec1bdd50479
Ion Leahu <forza11879@gmail.com>
2021-01-13 01:08:28 -0500

    update tweet controller

--- controllers/tweet.js
+++ controllers/tweet.js
@@ -7,6 +7,7 @@ export const getTweetPause = (req, res) => {
   console.log('getTweetPause');
   process.nextTick(() => twitterStream.destroy());
   // To stop a stream, call stream.destroy(). That might take a while though, if the stream receives a lot of traffic. Also, if you attempt to destroy a stream from an on handler, you may get an error about writing to a destroyed stream. In that case, try to defer the destroy() call: The solution for both errors is to defer the destroy() call to the next tick:
+  res.status(200).json({ message: 'ok' });
 };
 
 // Sets search term for twitter stream.
@@ -20,4 +21,5 @@ export const setSearchTerm = (req, res) => {
     twitterStream.destroy();
   }
   twitterStream = stream(term, req.app.locals.clients, twitterStream);
+  res.status(200).json({ message: 'ok' });
 };
